db8aa43a3d5ed8ce8fdbdc4036cacb8770820946
dianabee <dianabee16@gmail.com>
2023-05-22 20:31:58 -0700

    second commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var length = prompt("How many characters (between 8 and 128) would you like your password to contain?");
+
+
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
