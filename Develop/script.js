8f2c4ad1fd825339be1556642c381b275fdf2bad
dianabee <dianabee16@gmail.com>
2023-05-24 13:38:44 -0700

    third commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Definition of the characters
+var characters = ("");
+var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var lowercase = ("abcdefghijklmnopqrstuvwxyz");
+var number = ("0123456789");
+var special = (" !#$%&'()*+,-./:;<=>?@[\]^_`{|}~");
+
+// This is the function to generate a password
 function generatePassword() {
-  var length = prompt("How many characters (between 8 and 128) would you like your password to contain?");
+  var password = ("");
 
+// Generates a prompt for the password length
+  var length = parseInt(
+    prompt("How many characters (between 8 and 128) would you like your password to contain?")
+  );
 
-  return password;
+// If password length input doesn't follow the parameters, a warning will pop-up
+    while (isNaN(length) || length < 8 || length > 128) {
+      length = parseInt(
+        prompt("Invalid! Please enter a number between 8 and 128:")
+      );
+  }
+   
+// Password criteria for the user to choose in a confirm dialog
+   var includeLowercase = confirm("Would you like to use lowercase letters?");
+   var includeUppercase = confirm("Would you like to use uppercase letters?");
+   var includeNumber = confirm("Would you like to use numbers?");
+   var includeSpecialChars = confirm("Would you like to use special characters?");
+  
+// This will define a characters set based on the user choices
+  if (includeLowercase == true){
+    characters += lowercase;
+  }
+  if (includeUppercase == true){
+    characters += uppercase;
+  }
+  if (includeNumber == true){
+    characters += number;
+  }
+  if (includeSpecialChars == true){
+    characters += special;
+  }
+  else if (includeLowercase == false && includeUppercase == false && includeNumber == false && includeSpecialChars == false){
+    alert("Your password must have at least one character type. Let's start again!");
+    return;
+  }
+
+// Generates a password according to the selected criteria
+  var password = ("");
+  for (var i = 0; i < length; i++){
+    var randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+   return password;  
 }
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# 3_password_generator
+# Password Generator
+
+## Description
+
+This project presents a Password Generator which was developed with JavaScript to allow the user to generate a random and secure password, according to a criteria previously determined. 
+
+## License
+
+N/A
+
+## Links
+
+Used class material for reference as well as searches on: 
+https://www.google.com
+https://www.owasp.org/index.php/Password_special_characters
+
+## Deployed application
